2a6830492c7e9499a20e719ec5806b99b25af8ed
Simon <simon.sorg@student.hpi.de>
2023-05-02 20:25:56 +0200

    Fix pyannote to specific commit

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         for r in pkg_resources.parse_requirements(
             open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
         )
-    ] + ["pyannote.audio @ https://github.com/pyannote/pyannote-audio/archive/refs/heads/develop.zip"],
+    ] + ["pyannote.audio @ git+https://github.com/pyannote/pyannote-audio@11b56a137a578db9335efc00298f6ec1932e6317"],
     entry_points = {
         'console_scripts': ['whisperx=whisperx.transcribe:cli'],
     },
