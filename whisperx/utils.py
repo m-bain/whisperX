0767597bff8668a7a82bdaacc686141e445c4717
Simon Sorg <simon.sorg@student.hpi.de>
2023-08-17 14:18:16 +0200

    fix writer fail on segments 0

--- whisperx/utils.py
+++ whisperx/utils.py
@@ -226,6 +226,9 @@ class SubtitlesWriter(ResultWriter):
         max_line_width = 1000 if raw_max_line_width is None else raw_max_line_width
         preserve_segments = max_line_count is None or raw_max_line_width is None
 
+        if len(result["segments"]) == 0:
+            return
+
         def iterate_subtitles():
             line_len = 0
             line_count = 1
