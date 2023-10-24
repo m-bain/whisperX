b5221333409e8c994e0439a9bc2e671196fd3567
Justin Lin <justinwlin@gmail.com>
2023-10-24 18:42:14 -0400

    Update setup.py to be adaptive to platform

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 import os
-
+import platform
 import pkg_resources
 from setuptools import setup, find_packages
+
+
+def get_pyannote_audio_version():
+    machine = platform.machine()
+    system = platform.system()
+    version = "3.0.0" if machine == "aarch64" or system == "Darwin" else "3.0.1"
+    return version
+
 
 setup(
     name="whisperx",
@@ -19,10 +27,11 @@ setup(
         for r in pkg_resources.parse_requirements(
             open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
         )
-    ] + ["pyannote.audio==3.0.1"],
-    entry_points = {
-        'console_scripts': ['whisperx=whisperx.transcribe:cli'],
+    ]
+    + [f"pyannote.audio=={get_pyannote_audio_version()}"],
+    entry_points={
+        "console_scripts": ["whisperx=whisperx.transcribe:cli"],
     },
     include_package_data=True,
-    extras_require={'dev': ['pytest']},
+    extras_require={"dev": ["pytest"]},
 )
