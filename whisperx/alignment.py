9f41c49fe5ce84d7633191a4301ee818abd84586
David Martin Rius <0991592@gmail.com>
2023-11-16 11:43:36 +0100

    Add align model for catalan language.

--- whisperx/alignment.py
+++ whisperx/alignment.py
@@ -50,7 +50,8 @@ DEFAULT_ALIGN_MODELS_HF = {
     "ko": "kresnik/wav2vec2-large-xlsr-korean",
     "ur": "kingabzpro/wav2vec2-large-xls-r-300m-Urdu",
     "te": "anuragshas/wav2vec2-large-xlsr-53-telugu",
-    "hi": "theainerd/Wav2Vec2-large-xlsr-hindi"
+    "hi": "theainerd/Wav2Vec2-large-xlsr-hindi",
+    "ca": "softcatala/wav2vec2-large-xlsr-catala"
 }
 
 
