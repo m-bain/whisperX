8227807fa9e076901ea4b4fbbf79c9777a6f5e03
Max Bain <36994049+m-bain@users.noreply.github.com>
2024-01-02 19:36:36 -0700

    Delete build/lib/whisperx directory

--- build/lib/whisperx/__init__.py
+++
@@ -1,4 +0,0 @@
-from .transcribe import load_model
-from .alignment import load_align_model, align
-from .audio import load_audio
-from .diarize import assign_word_speakers, DiarizationPipeline
--- build/lib/whisperx/__main__.py
+++
@@ -1,4 +0,0 @@
-from .transcribe import cli
-
-
-cli()
