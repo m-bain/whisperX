bbaa2f0d1a1e98a8deae04968fc3f0fd6506cd35
Max Bain <36994049+m-bain@users.noreply.github.com>
2024-02-22 15:59:14 +0000

    update kwargs

--- whisperx/asr.py
+++ whisperx/asr.py
@@ -320,6 +320,9 @@ def load_model(whisper_arch,
         "prepend_punctuations": "\"'“¿([{-",
         "append_punctuations": "\"'.。,，!！?？:：”)]}、",
         "suppress_numerals": False,
+        "max_new_tokens": None,
+        "clip_timestamps": None,
+        "hallucination_silence_threshold": None,
     }
 
     if asr_options is not None:
