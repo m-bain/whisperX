8e2577c77c9863bc27518eb56b0e6d22e6a73cd9
Muhammad Ahsan <muhammad.ahsan1@afiniti.com>
2024-03-16 13:12:22 +0500

    removed new args as older version

--- whisperx/asr.py
+++ whisperx/asr.py
@@ -320,9 +320,6 @@ def load_model(whisper_arch,
         "prepend_punctuations": "\"'“¿([{-",
         "append_punctuations": "\"'.。,，!！?？:：”)]}、",
         "suppress_numerals": False,
-        "max_new_tokens": None,
-        "clip_timestamps": None,
-        "hallucination_silence_threshold": None,
     }
 
     if asr_options is not None:
