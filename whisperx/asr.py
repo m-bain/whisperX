a9a39b9c69adc3e20ffc6e5263c1694598aa18e1
Emmanuel Schmidbauer <eschmidbauer@gmail.com>
2024-08-22 13:33:48 -0400

    faster-whisper: update to latest version

--- whisperx/asr.py
+++ whisperx/asr.py
@@ -326,6 +326,7 @@ def load_model(whisper_arch,
         "max_new_tokens": None,
         "clip_timestamps": None,
         "hallucination_silence_threshold": None,
+        "hotwords": "",
     }
 
     if asr_options is not None:
