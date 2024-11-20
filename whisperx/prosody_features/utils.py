1134c913e37a87972da8e454af3105f20ae9ace8
nmehlman <nmehlman@usc.edu>
2024-11-20 15:15:16 -0800

    Add char filtering

--- whisperx/prosody_features/utils.py
+++ whisperx/prosody_features/utils.py
@@ -1,10 +1,13 @@
-def generate_char_frame_sequence(char_data, null_token: str ='<NULL>'):
+import string
+
+def generate_char_frame_sequence(char_data, null_token: str ='<NULL>', filter_non_char: bool = True):
     r"""
     Generate a list of characters for each frame
 
     Args:
         char_data (list of dict): List of dictionaries with 'char', 'start', and 'end'. Produced by alignmnet function's 'char' output.
-        null_token (str): token to assing for non-speech frames. Defaults to '<NULL>'.
+        null_token (str): Token to assing for non-speech frames. Defaults to '\<NULL>'.
+        filter_non_char (bool): Remove any non-letter symbols. Defaults to. Defaults to True.
         
     Returns:
         list of str: List of characters for each frame.
@@ -12,6 +15,10 @@ def generate_char_frame_sequence(char_data, null_token: str ='<NULL>'):
     
     # Standardize to lowercase
     char_data = [{**entry, 'char': entry['char'].lower()} for entry in char_data if 'start' in entry]
+    
+    # Remove non-letters
+    if filter_non_char:
+        char_data = [entry for entry in char_data in entry['char'] in string.ascii_lowercase]
     
     # Determine the total time range from the min start and max end
     min_time = int(min(entry['start'] for entry in char_data))
