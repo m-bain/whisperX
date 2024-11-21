1bca428c97d10b6e61b3c4721b45289f0bea0b1d
nmehlman <nmehlman@usc.edu>
2024-11-21 14:27:10 -0800

    Initial feature extraction code

--- whisperx/prosody_features/extract_features.py
+++ whisperx/prosody_features/extract_features.py
@@ -3,5 +3,6 @@ import os
 root = "/project/shrikann_35/nmehlman/vpc"
 
 for dirpath, dirnames, filenames in os.walk(root):
-    if 'wav' in dirnames:
+    
+    if 'wav' in dirpath: 
         print(dirpath)
