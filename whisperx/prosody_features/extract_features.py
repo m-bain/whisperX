fe0f0c1a033e6472a6f76ca065be3a86e354357d
nmehlman <nmehlman@usc.edu>
2024-11-21 16:15:43 -0800

    Debug

--- whisperx/prosody_features/extract_features.py
+++ whisperx/prosody_features/extract_features.py
@@ -25,7 +25,7 @@ def get_aligned_chars(audio_file: str, device: str = 'cpu') -> List[dict]:
 
     # 2. Align whisper output
     model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
-    result = whisperx.align_for_prosody_features(result["segments"], model_a, metadata, audio, device, return_char_alignments=False)
+    result = whisperx.align_for_prosody_features(result["segments"], model_a, metadata, audio, device, return_char_alignments=True)
 
     return result['char']
 
