d88d45013844c5b4f9689ba2f4460bc0a055edd7
nmehlman <nmehlman@usc.edu>
2024-11-22 15:42:18 -0800

    Started data code

--- whisperx/prosody_features/data.py
+++ whisperx/prosody_features/data.py
@@ -2,12 +2,18 @@ from torch.utils.data import Dataset, DataLoader, random_split
 import torch
 import os
 
+VC_SYSTEMS = ("B3", "B4", "B5", "T10-2", "T12-5", "T25-1",  "T8-5")
+
 class VPCDataset(Dataset):
     
-    def __init__(self, root_path: str, system: str = "all", split = "train"):
+    def __init__(self, root_path: str, system: str = "all", split = "train-clean-360"):
         
         self.paths = []
         self.speakers = []
         
-        
-        
+        if system == 'all': # Train on all VC systems
+            pass
+            
+        else:
+            pass
+            
