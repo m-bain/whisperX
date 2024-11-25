964f5c5157eb9953d9da600aa953c1451eee3e96
nmehlman <nmehlman@usc.edu>
2024-11-25 14:39:38 -0800

    Removed __init__ imports

--- whisperx/__init__.py
+++ whisperx/__init__.py
@@ -1,4 +1,4 @@
-from .transcribe import load_model
-from .alignment import load_align_model, align, align_for_prosody_features
-from .audio import load_audio
-from .diarize import assign_word_speakers, DiarizationPipeline
+#from .transcribe import load_model
+#from .alignment import load_align_model, align, align_for_prosody_features
+#from .audio import load_audio
+#from .diarize import assign_word_speakers, DiarizationPipeline
