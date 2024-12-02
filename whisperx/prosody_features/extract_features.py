eb0ce276efebc919a069da287f0f0d61b16be887
nmehlman <nmehlman@usc.edu>
2024-12-02 15:02:59 -0800

    Removed GT text

--- whisperx/prosody_features/extract_features.py
+++ whisperx/prosody_features/extract_features.py
@@ -1,7 +1,7 @@
 import os
 from typing import List
 import whisperx
-from whisperx.prosody_features.utils import generate_char_frame_sequence, parse_file_to_dict
+from whisperx.prosody_features.utils import generate_char_frame_sequence
 import gc
 import json
 import tqdm
@@ -67,23 +67,13 @@ if __name__ == "__main__":
         help="Type of compute format to use. Default: 'float16'.",
     )
 
-    parser.add_argument(
-        '--use_true_transcripts', 
-        action='store_true', 
-        help='Use ground-truth transcripts rather than Whipser predictions'
-    )
-
-
     args = parser.parse_args()
     root = args.root
     device = args.device
     compute_type = args.compute_type
-    use_true_transcripts = args.use_true_transcripts
 
     # Pre-load models
-    if not use_true_transcripts:
-        whisper_model = load_model("large-v2", device, compute_type=compute_type)
-
+    whisper_model = load_model("large-v2", device, compute_type=compute_type)
     alignment_model, alignmet_model_metadata = load_align_model(
         language_code="en", device=device
     )
@@ -95,9 +85,6 @@ if __name__ == "__main__":
         if "wav" in dirpath:
 
             save_dir = dirpath.replace("wav", "char_feats")
-            text_path = dirpath.replace("wav", "text")
-
-            text_dict = parse_file_to_dict(text_path)
 
             if not os.path.exists(save_dir):
                 os.mkdir(save_dir)
