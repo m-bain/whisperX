eee2664a89c7e8c59674f5a8a303f2c89f7a764d
Abdur Rahman Hatim <hatim@4gd.ai>
2024-12-12 12:11:14 +0530

    reverted to loading weights from huggingface

--- whisperx/diarize.py
+++ whisperx/diarize.py
@@ -16,7 +16,7 @@ class DiarizationPipeline:
     ):
         if isinstance(device, str):
             device = torch.device(device)
-        self.model = Pipeline.from_pretrained("app/whisperX/whisperx/assets/config.yaml").to(device)
+        self.model = Pipeline.from_pretrained(model_name, use_auth_token=use_auth_token).to(device)
 
     def __call__(self, audio: Union[str, np.ndarray], num_speakers=None, min_speakers=None, max_speakers=None):
         if isinstance(audio, str):
