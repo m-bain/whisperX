3791dd4e093b6d738a89172ddc58f57455905308
DaniilGumin <gumin.danya@gmail.com>
2025-02-11 22:40:28 +0300

    fix: setup

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="whisperx",
-    py_modules=["whisperx"],
-    version="3.3.1",
+    name="vox2me_whisperx",
+    py_modules=["vox2me_whisperx"],
+    version="1.0.1",
     description="Time-Accurate Automatic Speech Recognition using Whisper.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires=">=3.9, <3.13",
     author="Max Bain",
-    url="https://github.com/m-bain/whisperx",
+    url="https://github.com/vox2me/whisperX",
     license="BSD-2-Clause",
     packages=find_packages(exclude=["tests*"]),
     install_requires=[
