65eed17a5b19af119e2b5913b9dea4bf36bd7188
nmehlman <nmehlman@usc.edu>
2025-02-24 14:29:57 -0800

    Debug

--- scripts/run_export.sh
+++ scripts/run_export.sh
@@ -2,9 +2,6 @@
 
 # Usage: ./launch_script.sh --data-root /path/to/data --save-root /path/to/save --compute-type float32 --file-type wav --skip-existing
 
-# Set environment variables for distributed processing
-export MASTER_ADDR="127.0.0.1"
-export MASTER_PORT=12355  # Ensure this port is free
 export WORLD_SIZE=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l) # Count available GPUs
 
 # Check if GPUs are available
@@ -18,6 +15,6 @@ eval "$(conda shell.bash hook)"
 conda activate whisperx
 
 # Run the Python script using torch.multiprocessing spawn
-python -m torch.distributed.launch --nproc_per_node=$WORLD_SIZE ../whisperx/prosody_features/extract_prosody_features.py "$@"
+python -m ../whisperx/prosody_features/extract_prosody_features.py "$@"
 
 conda deactivate
