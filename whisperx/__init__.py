d856b29c1966625c58f3fc4b372034e515908b39
JoFrhwld <JoFrhwld@gmail.com>
2025-08-05 11:09:48 -0400

    changing __init__.py

--- whisperx/__init__.py
+++ whisperx/__init__.py
@@ -1,31 +1,12 @@
-import importlib
+from whisperx.alignment import load_align_model, align
+from whisperx.asr import load_model
+from whisperx.audio import load_audio
+from whisperx.diarize import assign_word_speakers
 
-
-def _lazy_import(name):
-    module = importlib.import_module(f"whisperx.{name}")
-    return module
-
-
-def load_align_model(*args, **kwargs):
-    alignment = _lazy_import("alignment")
-    return alignment.load_align_model(*args, **kwargs)
-
-
-def align(*args, **kwargs):
-    alignment = _lazy_import("alignment")
-    return alignment.align(*args, **kwargs)
-
-
-def load_model(*args, **kwargs):
-    asr = _lazy_import("asr")
-    return asr.load_model(*args, **kwargs)
-
-
-def load_audio(*args, **kwargs):
-    audio = _lazy_import("audio")
-    return audio.load_audio(*args, **kwargs)
-
-
-def assign_word_speakers(*args, **kwargs):
-    diarize = _lazy_import("diarize")
-    return diarize.assign_word_speakers(*args, **kwargs)
+__all__ = [
+    "load_align_model",
+    "align",
+    "load_model",
+    "load_audio",
+    "assign_word_speakers"
+]
